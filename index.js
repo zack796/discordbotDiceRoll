60a6e47a311456b52f75f67374722ab589cfd856
zack796 <zack796@gmail.com>
2018-02-27 17:51:34 -0600

    simplified
    removed commando, working with basic libraries.

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const Discord = require('discord.js');
-const commando = require('discord.js-commando');
-const bot = new commando.Client({
-  commandPrefix: '!',
-  owner: '100607191337164800'
-});
+//objects inside of parameters require {}
+const bot = new Discord.Client();
 
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-/*
-bot.on('message', (message)=> {
+bot.on('message', message => {
 
   if(message.content == '!ping') {
     message.channel.send("pong!");
   }
 });
-*/
+
 bot.login('NDE4MTU0Njk2MTA5MTk1Mjc0.DXdcbA.iZctTIhq7BlabEMs4mozgEPVzLc');
